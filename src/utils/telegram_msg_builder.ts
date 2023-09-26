80dc02dc5fa3fa3bc5cd5aebdb8b3ea4bfe7fae5
Heizi <xm.zhongjiaxin@gmail.com>
2023-09-26 21:18:11 +0800

    #format #3space

--- src/utils/telegram_msg_builder.ts
+++ src/utils/telegram_msg_builder.ts
@@ -2,39 +2,48 @@ import { ReplyMarkup } from '../telegram/types';
 
 
 export class TelegramMessageBuilder {
-  constructor(public data:string="") {}
-  line(text:string) {
-    this.data+=text+"\n"
-  }
-  b(text:string):string {
-    return `<b>${text}</b>`
-  }
-  i(text:string):string {
-    return `<i>${text}</i>`
-  }
-  code(text:string):string {
-    return `<code>${text}</code>`
-  }
-  del(text:string):string {
-    return `<del>${text}</del>`
-  }
-  pre(text:string):string {
-    return `<pre>${text}</pre>`
-  }
-  link(text:string,link:string):string {
-    return `<a href="${link}">${text}</a>`
-  }
-  subtitle = (title: string,s: string) => `${this.b(title)}: ${s}`
+   constructor(public data: string = '') {}
 
-  withMarkup(markup?:ReplyMarkup):SendMessageLike {
+   line(text: string) {
+      this.data += text + '\n';
+   }
+
+   b(text: string): string {
+      return `<b>${text}</b>`;
+   }
+
+   i(text: string): string {
+      return `<i>${text}</i>`;
+   }
+
+   code(text: string): string {
+      return `<code>${text}</code>`;
+   }
+
+   del(text: string): string {
+      return `<del>${text}</del>`;
+   }
+
+   pre(text: string): string {
+      return `<pre>${text}</pre>`;
+   }
+
+   link(text: string, link: string): string {
+      return `<a href='${link}'>${text}</a>`;
+   }
+
+   subtitle = (title: string, s: string) => `${this.b(title)}: ${s}`;
+
+   withMarkup(markup?: ReplyMarkup): SendMessageLike {
       return {
-         text:this.data,
-         keyboard:markup
-      }
-  }
+         text: this.data,
+         keyboard: markup
+      };
+   }
 
 }
+
 export type SendMessageLike = {
-   text:string,
-   keyboard?:ReplyMarkup
+   text: string,
+   keyboard?: ReplyMarkup
 }
